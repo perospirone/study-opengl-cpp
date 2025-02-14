47f5a0e0d7f931543a3dafc56a6f946580ae3ec9
perospirone <danidevpy@gmail.com>
2025-02-14 00:25:06 -0300

    hello window

--- main.cpp
+++ main.cpp
@@ -1,41 +1,85 @@
-#include <GL/glew.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
+#include <cstdio>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+void process_input(GLFWwindow* window);
 
 int main() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
+  if (!glfwInit()) {
+    return -1;
+  }
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    glfwMakeContextCurrent(window);
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }
+  int width = 800;
+  int height = 600;
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glViewport(0, 0, 800, 600);
+  printf("window width %i\n", width);
+  printf("window height %i\n", height);
 
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
+  GLFWwindow *window =
+      glfwCreateWindow(width, height, "Hello World", NULL, NULL);
+  if (!window) {
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    return -1;
+  }
+
+  glViewport(0, 0, width, height);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+  glfwSetKeyCallback(window, key_callback);
+
+  // Main loop
+  while (!glfwWindowShouldClose(window)) {
+    // Render here
+    //glClear(GL_COLOR_BUFFER_BIT);
+
+    // glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    process_input(window);
+
+    // Swap front and back buffers
+    glfwSwapBuffers(window);
+
+    // Poll for and process events
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  printf("key: %s\n", glfwGetKeyName(key, scancode));
+  if (key == GLFW_KEY_E && action == GLFW_PRESS) {
+    printf("key E");
+  }
+}
+
+void process_input(GLFWwindow *window) {
+  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+
+  if(glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
+    printf("press r\n");
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+  }
 }
