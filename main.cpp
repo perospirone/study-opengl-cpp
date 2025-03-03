ee2e2c95e668dbd8458ce59e99567da60641b74e
perospirone <danidevpy@gmail.com>
2025-03-02 23:08:38 -0300

    shaders

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+#include <cmath>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <cstdio>
+#include <iostream>
+#include "shader_s.h"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
@@ -9,17 +12,21 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action,
 
 void process_input(GLFWwindow *window);
 
-const char *vertex_shader = "#version 330 core\n"
-                            "in vec3 vp;"
-                            "void main() {"
-                            "  gl_Position = vec4( vp, 1.0 );"
-                            "}";
+//const char *vertex_shader = "#version 330 core\n"
+                            //"layout (location = 0) in vec3 a_pos;"
+                            //"layout (location = 1) in vec3 a_color;"
+                            //"out vec3 our_color;"
+                            //"void main() {"
+                            //"  gl_Position = vec4(a_pos, 1.0);"
+                            //"  our_color = a_color;"
+                            //"}";
 
-const char *fragment_shader = "#version 330 core\n"
-                              "out vec4 frag_colour;"
-                              "void main() {"
-                              "  frag_colour = vec4( 0.5, 0.0, 0.5, 1.0 );"
-                              "}";
+//const char *fragment_shader = "#version 330 core\n"
+                              //"out vec4 frag_colour;"
+                              //"in vec3 our_color;"
+                              //"void main() {"
+                              //"  frag_colour = vec4(our_color, 1.0);"
+                              //"}";
 
 int main() {
   if (!glfwInit()) {
@@ -52,10 +59,18 @@ int main() {
   glViewport(0, 0, width, height);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  float triangle[] = {0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f};
+  //float triangle[] = {0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f};
 
-  float square[] = {-0.5f, 0.5f,  0.0f, 0.5f,  0.5f,  0.0f,
-                    0.5f,  -0.5f, 0.0f, -0.5f, -0.5f, 0.0f};
+  //float square[] = {-0.5f, 0.5f,  0.0f, 0.5f,  0.5f,  0.0f,
+                    //0.5f,  -0.5f, 0.0f, -0.5f, -0.5f, 0.0f};
+
+
+  float triangle[] = {
+    // positions         // colors
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
+  }; // idk why the opengl tutorial put color inside vertices array and no is a other array
 
   unsigned int vao1, vbo1;
 
@@ -65,49 +80,28 @@ int main() {
   glBindVertexArray(vao1);
   glBindBuffer(GL_ARRAY_BUFFER, vbo1);
   glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+  glEnableVertexAttribArray(1);
   glBindVertexArray(0);
 
-  unsigned int vao2, vbo2;
-  glGenVertexArrays(1, &vao2);
-  glGenBuffers(1, &vbo2);
-
-  glBindVertexArray(vao2);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(square), square, GL_STATIC_DRAW);
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
-  glBindVertexArray(0);
-
-  unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vs, 1, &vertex_shader, NULL);
-  glCompileShader(vs);
-
-  unsigned int fs = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fs, 1, &fragment_shader, NULL);
-  glCompileShader(fs);
-
-  unsigned int shader_program = glCreateProgram();
-  glAttachShader(shader_program, vs);
-  glAttachShader(shader_program, fs);
-  glLinkProgram(shader_program);
-
-  glDeleteShader(vs);
-  glDeleteShader(fs);
+  Shader ourShader("vertex.glsl", "fragment.glsl");
 
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
     process_input(window);
-    glUseProgram(shader_program);
+    ourShader.use();
 
-    // Render Triangle
+    // render triangle
     glBindVertexArray(vao1);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    // Render Square
-    glBindVertexArray(vao2);
-    glDrawArrays(GL_LINE_LOOP, 0, 4); // Use GL_TRIANGLE_FAN for filled square
+    // render square
+    //glBindVertexArray(vao2);
+    //glDrawArrays(GL_LINE_LOOP, 0, 4); // Use GL_TRIANGLE_FAN for filled square
 
     glfwSwapBuffers(window);
     glfwPollEvents();
